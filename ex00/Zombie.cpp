c0d80cf434598a1cf362f46461d7aaf55981d20e
pdrlrnc <pedrolourencoemail@gmail.com>
2026-02-05 17:26:04 +0000

    ex00 done

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -1,10 +1,16 @@
 #include "Zombie.hpp"
+#include <iostream>
 
 Zombie::Zombie(std::string name): _name(name) {}
 
-Zombie::~Zombie(){}
+Zombie::~Zombie()
+{
+	std::cout
+		<< _name << " has left the chat." << std::endl;
+}
 
 void Zombie::announce(void)
 {
-	std::cout << _name << ":  BraiiiiiiinnnzzzZ...";
+	std::cout 
+		<< _name << ":  BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,3 +1,36 @@
+#include "Zombie.hpp"
+#include <iostream>
+
+
+/* prototypes */
+Zombie *newZombie(std::string name);
+void randomChump(std::string name);
+
 int main ()
 {
+	std::cout 
+		<< "This zombie is gonna be on the stack (Stan)" << std::endl;
+	randomChump("Stan");
+
+	std::cout
+		<< "This zombie is gonna be on the heap (Hephaestus)" << std::endl;
+
+	Zombie *hep;
+	try 
+	{
+		hep = newZombie("Hephaestus");
+	} catch (const std::bad_alloc& e)
+	{
+		std::cout 
+			<< "Memory allocation failed. Exiting..." << std::endl;
+		return (1);
+	}
+	
+	std::cout
+		<< "Get ready for Hephaestus' announcement!!" << std::endl;
+	hep->announce();
+	std::cout
+		<< "That was exciting! Say your goodbies to Hephaestus." << std::endl;
+	delete hep;
+	return (0);
 }
